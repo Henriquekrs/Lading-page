9ce864fdb9b149bbf793a09d415f71241fa4cabe
Gustavo Henrique R Duarte <ghrduarte@hotmail.com>
2024-12-17 19:02:08 -0300

    styling adjustments

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,49 +1,27 @@
-import { useContext } from 'react';
 import { InputsLogin } from '../components/InputsLogin';
-import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
-import myContext from '../context/context';
+import styled from 'styled-components';
+
 import { ButtonLightOrDark } from '../components/ButtonLightOrDark';
-
-const lightTheme = {
-  background: '#a8dadc',
-};
-
-const darkTheme = {
-  background: '#0d1321',
-};
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    background-color: ${(props) => props.theme.background};
-    font-family: 'Alumni Sans Pinstripe', sans-serif;
-
-    transition: background-color 0.5s ease; /* Transição suave */
-  }
-`;
+import { TitlePage } from '../components/TitlePage';
+import { Header } from '../components/Header';
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 100vw;
   height: 100vh;
-  background: ${(props) => props.theme.background};
+  background: linear-gradient(270deg, #3c0000 0%, #000000 50%, #008f11 115%);
   justify-content: center;
   align-items: center;
-
-  transition: background-color 0.5s ease; /* Transição suave */
 `;
 
 export function Login() {
-  const { darkMode } = useContext(myContext).value;
   return (
-    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-      <GlobalStyle />
-      <Container>
-        <InputsLogin />
-        <ButtonLightOrDark />
-      </Container>
-    </ThemeProvider>
+    <Container>
+      <Header />
+      <InputsLogin />
+      <ButtonLightOrDark />
+      <TitlePage />
+    </Container>
   );
 }
